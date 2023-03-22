3dbf05730797202cee3dd4c20860c4794173e199
Leah <leahxharden@gmail.com>
2023-03-21 23:41:33 -0400

    moving files and add switch

--- server.js
+++ server.js
@@ -14,12 +14,13 @@ const PORT = 3001;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
-
+app.get('/', (req, res) =>  
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 //switch pages
 app.get('/notes', (req, res) =>  
-    res.sendFile(path.join(__dirname, '/index.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 //add
